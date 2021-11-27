eb76f506fddd12272e5f12ca455752b2f5275661
Karthik Mohanasundaram <connect2kms@outlook.com>
2021-11-27 01:24:35 -0500

    Custom pipes added

--- src/app/convert.pipe.ts
+++ src/app/convert.pipe.ts
@@ -1,12 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'convert'
+  name: 'convert',
 })
 export class ConvertPipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: number, targetUnit: string): any {
+    switch (targetUnit) {
+      case 'km':
+        return value * 1.609;
+      case 'm':
+        return value * 1.609 * 1000;
+      case 'cm':
+        return value * 1.609 * 1000 * 1000;
+      default:
+        throw new Error('Target Value Not Supported');
+    }
   }
-
 }
