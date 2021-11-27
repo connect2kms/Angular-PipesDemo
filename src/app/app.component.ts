3f450bf028d8aac2b3eebbab35b60420ceb2f0a3
Karthik Mohanasundaram <connect2kms@outlook.com>
2021-11-27 00:37:27 -0500

    Added Payee Name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'PipesDemo';
+  name: string = '';
+  date: string = '';
+
+  onNameChange(event: Event) {
+    this.name = (event.target as HTMLInputElement).value;
+  }
+
+  onDateChange(event: Event) {
+    this.date = (event.target as HTMLInputElement).value;
+  }
 }
